7a46bf113fe3eb4302fce4c4a96a9eafc4ae8d92
FameZeeshan <zeeshan6143@gmail.com>
2023-07-05 22:06:18 +0530

    4

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="main-container">
+    <div className="main-container m-3">
       <h1>About</h1>
       <h3>I'm Mohammed Zeeshan</h3>
       <p>
